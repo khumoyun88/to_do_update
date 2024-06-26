e5b128c16e842c392d4eb122de42ecd5d59f73ff
khumoyun88 <davlatovkhumoyun@gmail.com>
2024-06-27 07:57:07 +1400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ToDO from './ToDo.jsx'
+// import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToDO />
+  </React.StrictMode>,
+)
